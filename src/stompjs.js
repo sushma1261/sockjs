dd792b7ca487656c5821a0f84460e5951b6cac91
sushma1261 <39758057+sushma1261@users.noreply.github.com>
2025-01-20 11:08:56 -0800

    added token in url

--- src/stompjs.js
+++ src/stompjs.js
@@ -1,67 +1,24 @@
 import { Client } from "@stomp/stompjs";
 import "./style.css";
-const token =
-  "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1NjcwNDY1MS1mYjMyLTQ0MjctOGZhNi1jYmZmNTI2YTg3NWQiLCJpYXQiOjE3MzczMzY4NjIsImV4cCI6MTczNzk0MTY2Mn0.nugMjgooq5Ad8MMmFz9SX0SEl7cXIIi7N5emO1aB06o";
-const serverUrl = "ws://localhost:8080/ws";
+
 let retryCount = 0;
 const maxRetries = 3;
 
 const inputBox = document.getElementById("input-box");
+const token = document.getElementById("token");
+const useToken = document.getElementById("use-token");
 const inputContent = document.getElementById("input-content");
 const inputPublishPath = document.getElementById("input-path-publish");
 
+const connectButton = document.getElementById("connect");
+const disconnectButton = document.getElementById("disconnect");
 const subscribeButton = document.getElementById("subscribe");
 const publishButton = document.getElementById("publish");
 const messages = document.getElementById("messages");
-
-const client = new Client({
-  brokerURL: serverUrl,
-  // webSocketFactory: () => {
-  //   const socket = new WebSocket(serverUrl, ["Authorization"], {
-  //     Headers: {
-  //       Authorization: `Bearer ${token}`,
-  //       custom: "test",
-  //       "Sec-Fetch-Site": "same-site", // Ensure appropriate fetch site context (Same-site)
-  //       "Sec-Fetch-Mode": "cors",
-  //     },
-  //   });
-  //   socket.withCredentials = true; // Ensure cookies/auth headers are sent
-  //   return socket;
-  // },
-  connectHeaders: {
-    Authorization: `Bearer ${token}`,
-    custom: "test",
-    "Sec-Fetch-Site": "same-site", // Ensure appropriate fetch site context (Same-site)
-    "Sec-Fetch-Mode": "cors",
-  },
-  // webSocketFactory: () => new SockJS("http://localhost:8080/ws"),
-
-  onConnect: () => {
-    console.log("Connected to WebSocket");
-    retryCount = 0; // Reset retry count on successful connection
-    const frag = document.createDocumentFragment();
-    const div = document.createElement("div");
-    div.innerHTML = "Connected...";
-    frag.appendChild(div);
-    messages.appendChild(frag);
-    // // Subscribe to a destination topic (modify topic as per your backend)
-    // client.subscribe("/ping", (message) => {
-    //   console.log(`Received: ${message.body}`);
-    // });
-  },
-  onDisconnect: () => {
-    console.log("Disconnected from WebSocket");
-  },
-  onStompError: (frame) => {
-    console.log(`Broker Error: ${frame.headers["message"]}`);
-    attemptReconnect();
-  },
-  onWebSocketError: (error) => {
-    client.connectHeaders;
-    console.log(`WebSocket Error: ${error}`, client.connectHeaders);
-    attemptReconnect();
-  },
-});
+const jsonError = document.getElementById("jsonError");
+const serverUrl = token.value
+  ? "ws://localhost:8080/ws?token=" + token.value
+  : "ws://localhost:8080/ws";
 
 subscribeButton.addEventListener("click", function () {
   const inputValue = inputBox.value;
@@ -93,7 +50,6 @@ publishButton.addEventListener("click", function () {
   messages.appendChild(frag);
   client.publish({
     destination: inputValue,
-    // body: { messages: content },
     headers: {
       "content-type": "application/json",
     },
@@ -127,4 +83,60 @@ publishButton.addEventListener("click", function () {
 // attemptReconnect();
 // Activate the client initially
 
-client.activate();
+connectButton.addEventListener("click", function () {
+  console.log(token.value);
+  client.brokerURL = useToken.checked
+    ? "ws://localhost:8080/ws?token=" + token.value
+    : "ws://localhost:8080/ws";
+  client.activate();
+});
+
+disconnectButton.addEventListener("click", function () {
+  console.log("Disconnecting");
+  client.deactivate();
+});
+
+const client = new Client({
+  connectHeaders: {
+    Authorization: `Bearer ${token}`,
+    custom: "test",
+    "Sec-Fetch-Site": "same-site", // Ensure appropriate fetch site context (Same-site)
+    "Sec-Fetch-Mode": "cors",
+  },
+  // webSocketFactory: () => new SockJS("http://localhost:8080/ws"),
+
+  onConnect: () => {
+    console.log("Connected to WebSocket");
+    retryCount = 0; // Reset retry count on successful connection
+    const frag = document.createDocumentFragment();
+    const div = document.createElement("div");
+    div.innerHTML = "Connected...";
+    frag.appendChild(div);
+    messages.appendChild(frag);
+    updateStatus(true);
+  },
+  onDisconnect: () => {
+    console.log("Disconnected from WebSocket");
+    updateStatus(false);
+  },
+  onStompError: (frame) => {
+    console.log(`Broker Error: ${frame.headers["message"]}`);
+    attemptReconnect();
+  },
+  onWebSocketError: (error) => {
+    client.connectHeaders;
+    console.log(`WebSocket Error: ${error}`, client.connectHeaders);
+    attemptReconnect();
+  },
+});
+
+function updateStatus(isConnected) {
+  const statusDiv = document.getElementById("status");
+  if (isConnected) {
+    statusDiv.textContent = "Status: Connected";
+    statusDiv.className = "connected";
+  } else {
+    statusDiv.textContent = "Status: Disconnected";
+    statusDiv.className = "disconnected";
+  }
+}
